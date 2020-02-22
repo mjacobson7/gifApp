48c6038d34612ea597a1147a45a08d55ff272b86
Max Jacobson <mjacobson7@gmail.com>
2020-02-22 10:50:08 -0700

    Update api request to https

--- public/mainService.js
+++ public/mainService.js
@@ -5,7 +5,7 @@ angular.module('gifApp')
       console.log(item);
       return $http ({
         method: 'GET',
-        url: 'http://api.giphy.com/v1/gifs/search?q=' + item + '&api_key=dc6zaTOxFJmzC'
+        url: 'https://api.giphy.com/v1/gifs/search?q=' + item + '&api_key=dc6zaTOxFJmzC'
       }).then(function(response) {
         return response;
       })
